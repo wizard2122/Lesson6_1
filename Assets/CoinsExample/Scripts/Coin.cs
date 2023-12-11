82800e5747af9a5b2d1c1f4d9c845fcbd1dd7b2e
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-12-11 10:49:42 +0300

    final

---
+++ Assets/CoinsExample/Scripts/Coin.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public abstract class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out ICoinPicker coinPicker))
+        {
+            Debug.Log("Проигрывается музыка подбора монетки");
+            Debug.Log("Проигрывается анимация");
+
+            AddCoins(coinPicker);
+
+            Destroy(gameObject);
+        }
+    }
+
+    protected abstract void AddCoins(ICoinPicker coinPicker);
+}
---
+++ FrontSearchPattern.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontSearchPattern : ICharacterFinder
+{
+    private Transform _center;
+    private float _viewingRange;
+
+    public IEnumerable<Character> Find()
+    {
+        throw new System.NotImplementedException();
+    }
+}
